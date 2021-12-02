b36a2677d185e3925f24836922b0b7afb43c6fe8
jmbarbone <jmbarbone@gmail.com>
2021-12-02 09:42:02 -0500

    adds day 1 part 2 python solution

--- 2021/Python/day-01.py
+++ 2021/Python/day-01.py
@@ -2,16 +2,31 @@
 import numpy as np
 import unittest
 
-def day_01() :
-  x = np.loadtxt("data/day-01.txt", dtype=int)
+x = np.loadtxt("data/day-01.txt", dtype=int)
+
+
+def day_01a() :
   n = len(x)
-  res = sum(x[range(0, n - 1)] < x[range(1, n)])
+  res = sum(x[:n - 1] < x[1:])
+  return res
+
+
+def day_01b() :
+  # adapted from https://stackoverflow.com/a/54628145/12126576
+  # need to ignore the first value
+  y = np.convolve(x[1:], np.ones(3), mode="same")
+  n = len(y)
+  res = sum(y[:n - 1] < y[1:])
   return res
 
 
 class testDay01(unittest.TestCase) :
-  def test_day_01(self) :
-    self.assertEqual(day_01(), 1696)
+  def test_day_01a(self) :
+    self.assertEqual(day_01a(), 1696)
   
+  def test_day_01b(self) :
+    self.assertEqual(day_01b(), 1737)
+  
+
 if __name__ == '__main__':
   unittest.main()
