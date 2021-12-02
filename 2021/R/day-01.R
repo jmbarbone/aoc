81ebcd8cde5d11df6716913876ba9809262ccf15
jmbarbone <jmbarbone@gmail.com>
2021-12-02 09:41:56 -0500

    adds day 1 part 2 R solution

--- 2021/R/day-01.R
+++ 2021/R/day-01.R
@@ -1,8 +1,18 @@
 
-day_01 <- function() {
-  x <- as.integer(readLines("data/day-01.txt"))
+x <- as.integer(readLines("data/day-01.txt"))
+
+day_01a <- function(x) {
   n <- length(x)
   sum(x[seq(1, n - 1)] < x[seq(2, n)])
 }
 
-stopifnot(identical(day_01(), 1696L))
+day_01b <- function(x) {
+  n <- length(x)
+  y <- RcppRoll::roll_sum(x, 3)
+  sum(y[seq(1, n - 1)] < y[seq(2, n)], na.rm = TRUE)
+}
+
+stopifnot(
+  identical(day_01a(x), 1696L),
+  identical(day_01b(x), 1737L)
+)
