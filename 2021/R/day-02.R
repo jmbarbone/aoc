d0114541d2c067a3b7ae3b2471e8af8cde1e6971
jmbarbone <jmbarbone@gmail.com>
2021-12-03 08:58:51 -0500

    adds day 2 R solution

---
+++ 2021/R/day-02.R
@@ -0,0 +1,33 @@
+
+x <- readLines("data/day-02.txt")
+x <- strsplit(x, "\\s")
+s <- seq_along(x)
+steps <- vapply(s, \(i) as.integer(x[[i]][2]), NA_integer_)
+names(steps) <- vapply(s, \(i) x[[i]][1], NA_character_)
+sums <- tapply(steps, names(steps), sum)
+res <- (sums[["down"]] - sums[["up"]]) * sums[["forward"]]
+
+stopifnot(identical(res, 1459206L))
+
+aim <- 0L
+horizontal <- 0L
+depth <- 0L
+
+for (i in s) {
+  switch(
+    names(steps[i]),
+    down = {
+      aim <- aim + steps[[i]]
+    },
+    up = {
+      aim <- aim - steps[[i]]
+    },
+    forward = {
+      horizontal <- horizontal + steps[[i]]
+      depth <- depth + aim * steps[[i]]
+    }
+  )
+}
+
+res <- horizontal * depth
+stopifnot(identical(position, 1320534480L))
