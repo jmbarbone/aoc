adc96d33116b696d16588580fd33fce9122e5f1b
jmbarbone <jmbarbone@gmail.com>
2021-12-06 00:17:59 -0500

    adds day 05 part 1 R

---
+++ 2021/R/day-05.R
@@ -0,0 +1,53 @@
+
+
+#  x1, y1 ->  x2, y2
+# 565,190 -> 756,381
+# .. is transformed to ...
+#     x   y
+# 1 565 190
+# 2 756 381
+
+intersect_points <- function(m) {
+  # if (is_horizontal) m
+  out <- apply(m, 2, \(i) seq(i[1], i[2]))
+  
+  if (any(lengths(out) == 1L)) {
+    out <- as.matrix(expand.grid(out))
+  } else {
+    stop("haven't made it this far yet")
+  }
+  
+  out
+}
+
+is_horizontal <- function(m) {
+  (m[1] == m[2]) | (m[3] == m[4])
+}
+
+x <- 
+  readLines("data/day-05.txt") |>
+  strsplit(" -> ") |>
+  lapply(\(i) vapply(strsplit(i, ","), as.integer, integer(2))) |>
+  lapply(\(i) {
+    dimnames(i) <- list(c("x", "y"), 1:2)
+    t(i)
+  })
+
+head(x)
+
+# get only horizontal lines
+
+are_horizontal <- vapply(x, is_horizontal, NA)
+m <- x[are_horizontal][[1]]
+
+
+n <- max(unlist(x))
+board <- matrix(0L, nrow = n, ncol = n)
+
+for (m in x[are_horizontal]) {
+  pos <- intersect_points(m)
+  board[pos] <- board[pos] + 1L
+}
+
+res <- sum(board >= 2)
+stopifnot(res == 8622)
