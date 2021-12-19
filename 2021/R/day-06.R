b7c4d11e58f9c46507452ce5269d544fd8737379
jmbarbone <jmbarbone@gmail.com>
2021-12-19 00:55:13 -0500

    Create day-06.R

---
+++ 2021/R/day-06.R
@@ -0,0 +1,51 @@
+x <- as.integer(strsplit(readLines("data/day-06.txt"), ",")[[1]])
+
+day_06x <- function(x) {
+  # Not great
+  for (i in 1:80) {
+    x <- x - 1L
+    new <- x == -1L
+    
+    if (any(new)) {
+      # just make it 9 instead of 8
+      x[new] <- 6L
+      x <- c(x, rep(8L, sum(new)))
+    }
+  }
+  
+  length(x)
+}
+
+
+day_06a <- function(x) {
+  do_day_06_solution(x, 80)
+}
+
+day_06b <- function(x) {
+  do_day_06_solution(x, 256)
+}
+ 
+do_day_06_solution <- function(x, n) {
+  counts <- rep(0, 9)
+  names(counts) <- 0:8
+  tab <- table(x)
+  counts[names(tab)] <- tab
+  
+  for (i in seq_len(n)) {
+    zeros <- counts[1]
+    counts <- c(counts[-1], counts[1])
+    counts[7] <- counts[7] + zeros
+  }
+  
+  sum(counts)
+}
+
+
+# test --------------------------------------------------------------------
+
+
+stopifnot(
+  day_06x(x) == 388739,
+  day_06a(x) == 388739,
+  day_06b(x) == 1741362314973
+)
