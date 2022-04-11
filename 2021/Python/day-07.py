ddccdd8cd723df59077aad543149a730903b528f
jmbarbone <jmbarbone@gmail.com>
2022-04-10 22:02:11 -0400

    add python solution 07

--- 2021/Python/day-07.py
+++ 2021/Python/day-07.py
@@ -3,13 +3,27 @@ import unittest
 
 
 def day_07a(x) :
-    x = None
-    return x
+    m = np.median(x)
+    return sum([abs(i - m) for i in x])
 
 
 def day_07b(x) :
-    x = None
-    return x
+    unique = np.unique(x)
+    ans = np.inf
+
+    for u in unique :
+        diffs = [abs(i - u) for i in x]
+        res = [iterative_cumsum(i) for i in diffs]
+        res = sum(res)
+
+        if res < ans :
+            ans = res
+
+    return ans
+
+
+def iterative_cumsum(n) :
+    return (n**2 + n) / 2
 
 
 class testDay07(unittest.TestCase) :
@@ -22,5 +36,5 @@ class testDay07(unittest.TestCase) :
   
 
 if __name__ == '__main__':
-    x = np.loadtxt("data/day-7.txt", dtype=int, delimiter=",")
+    x = np.loadtxt("data/day-07.txt", dtype=int, delimiter=",")
     unittest.main()
