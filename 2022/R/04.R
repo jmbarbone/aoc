7a024a28cedbb15f2e57e63ed18b5605ec0c11fa
Jordan Mark Barbone <jmbarbone@gmail.com>
2022-12-04 16:14:11 -0500

    add day 4 R solution

---
+++ 2022/R/04.R
@@ -0,0 +1,49 @@
+
+
+# solve -------------------------------------------------------------------
+
+df <- read.table("2022/data/04", sep = ",", col.names = c("x", "y"))
+
+parse <- function(x) {
+  lapply(strsplit(x, "-"), as.integer)
+}
+
+fully_contained <- function(x, y) {
+  x <- parse(x)
+  y <- parse(y)
+  mapply(function(a, b) {
+    (a[1] <= b[1] & a[2] >= b[2]) | (a[1] >= b[1] & a[2] <= b[2])
+  },
+  a = x,
+  b = y)
+
+}
+
+solution1 <- sum(with(df, fully_contained(x, y)))
+
+overlapped <- function(x, y) {
+  x <- parse(x)
+  y <- parse(y)
+  mapply(function(a, b) {
+
+    FALSE |
+      (a[1] >= b[1] & a[1] <= b[2]) |
+      (b[1] >= a[1] & b[1] <= a[2]) |
+      (a[2] >= b[1] & a[2] <= b[2]) |
+      (b[2] >= a[1] & b[2] <= a[2]) |
+      FALSE
+  },
+  a = x,
+  b = y)
+
+}
+
+solution2 <- NULL
+
+# test --------------------------------------------------------------------
+
+solutions <- readLines("2022/solutions/04")
+stopifnot(
+  solution1 == solutions[1],
+  solution2 == solutions[2]
+)
